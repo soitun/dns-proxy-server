4571fde9e89ac39410a58d87cf9981a4cf7ee5e4
Elvis de Freitas <edigitalb@gmail.com>
2023-01-29 13:50:25 -0300

    setup dnsjava lib

--- src/main/java/com/mageddo/dnsproxyserver/App.java
+++ src/main/java/com/mageddo/dnsproxyserver/App.java
@@ -1,7 +1,17 @@
 package com.mageddo.dnsproxyserver;
 
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+
+    // start webserver
+
+    // start dns server
+
+    // configurations
+
+    //  setup as default dns
+    //  install as service
+
+  }
 }
