d699dca9e9a03a81773a6e56409655a512e35f94
Elvis de Freitas <edigitalb@gmail.com>
2023-01-28 21:27:10 -0300

    reverting files

--- builder.bash
+++ builder.bash
@@ -4,6 +4,7 @@ set -e
 
 CUR_DIR=`pwd`
 APP_VERSION=$(cat VERSION)
+REPO_URL=mageddo/dns-proxy-server
 
 
 assemble(){
@@ -21,7 +22,6 @@ assemble(){
 
 generateDocs(){
 	rm -r ${TARGET} || echo "not exists ${TARGET}"
-	mkdir ${TARGET}
 	hugo --baseURL=http://mageddo.github.io/dns-proxy-server/$1 \
 	--destination $2 \
 	--ignoreCache --source docs/
@@ -29,11 +29,37 @@ generateDocs(){
 
 case $1 in
 
+	setup-repository )
+		git remote remove origin  && git remote add origin https://${REPO_TOKEN}@github.com/$REPO_URL.git
+		git checkout -b build_branch ${CURRENT_BRANCH}
+		echo "> Repository added, travisBranch=${CURRENT_BRANCH}"
+
+	;;
+
+	upload-release )
+
+		DESC=$(cat RELEASE-NOTES.md | awk 'BEGIN {RS="|"} {print substr($0, 0, index(substr($0, 3), "###"))}' | sed ':a;N;$!ba;s/\n/\\r\\n/g')
+		github-cli release mageddo dns-proxy-server $APP_VERSION $CURRENT_BRANCH "${DESC}" $PWD/build/*.tgz
+
+	;;
+
 	docs )
 
 	VERSION=$(cat VERSION | awk -F '.' '{ print $1"."$2}');
-	TARGET=$PWD/.docs
+	TARGET=$PWD/../dns-proxy-server-docs/${VERSION}
 	generateDocs ${VERSION} ${TARGET}
+
+	VERSION=latest
+	TARGET=$PWD/../dns-proxy-server-docs/${VERSION}
+	generateDocs ${VERSION} ${TARGET}
+
+	;;
+
+	apply-version )
+
+		# updating files version
+		sed -i -E "s/(dns-proxy-server.*)[0-9]+\.[0-9]+\.[0-9]+/\1$APP_VERSION/" docker-compose.yml
+		sed -i -E "s/[0-9]+\.[0-9]+\.[0-9]+/$APP_VERSION/g" Dockerfile*.hub
 
 	;;
 
@@ -74,6 +100,15 @@ case $1 in
 		tar --exclude=*.tgz -czf $TAR_FILE *
 	;;
 
+	validate-release )
+
+		if git rev-parse "$APP_VERSION^{}" >/dev/null 2>&1; then
+			echo "> Version already exists $APP_VERSION"
+			exit 3
+		fi
+
+	;;
+
 	deploy-ci )
 
 	EC=0
@@ -83,15 +118,24 @@ case $1 in
 	elif [ "$EC" -ne "0" ]; then
 		exit $EC
 	fi
-
-	docker-compose build prod-build-image-dps prod-build-image-dps-arm7x86 prod-build-image-dps-arm8x64
+	docker-compose build prod-build-image-dps-arm7x86 prod-build-image-dps-arm8x64 &&\
+	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin &&\
+	docker-compose push prod-build-image-dps-arm7x86 prod-build-image-dps-arm8x64
 
 	;;
 
 	release )
 
-		echo "> building new version"
-		builder.bash build
+		echo "> build started, current branch=$CURRENT_BRANCH"
+		if [ "$CURRENT_BRANCH" = "master" ]; then
+			echo "> deploying new version"
+			builder.bash validate-release && builder.bash apply-version && builder.bash build && builder.bash upload-release
+
+		else
+			echo "> building candidate"
+			builder.bash validate-release
+			builder.bash build
+		fi
 
 	;;
 
