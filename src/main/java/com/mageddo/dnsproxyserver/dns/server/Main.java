a7c4359af80ad1d48aecc99923c9e52d5c7ad730
Elvis de Freitas <edigitalb@gmail.com>
2023-01-29 16:28:32 -0300

    Add some writer threads to udp server

--- src/main/java/com/mageddo/dnsproxyserver/dns/server/Main.java
+++ src/main/java/com/mageddo/dnsproxyserver/dns/server/Main.java
@@ -6,10 +6,21 @@ import lombok.SneakyThrows;
 public class Main {
   @SneakyThrows
   public static void main(String[] args) {
+    final var factory = Factory.factory();
 
-    Factory
-        .factory()
-        .dnsServerStarter()
-        .start();
+    // start dns server
+    factory.dnsServerStarter().start();
+
+    // start webserver
+
+    // configurations
+
+    //  setup as default dns
+
+    //  install as service
+
+    Thread.currentThread().join();
+
+
   }
 }
--- src/main/java/com/mageddo/dnsproxyserver/dns/server/UDPServer.java
+++ src/main/java/com/mageddo/dnsproxyserver/dns/server/UDPServer.java
@@ -1,6 +1,8 @@
 package com.mageddo.dnsproxyserver.dns.server;
 
 import com.mageddo.dnsproxyserver.dns.server.solver.Solver;
+import com.mageddo.dnsproxyserver.threads.ThreadPool;
+import lombok.extern.slf4j.Slf4j;
 import org.xbill.DNS.Message;
 
 import java.io.IOException;
@@ -10,13 +12,17 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
+@Slf4j
 public class UDPServer {
   public static final short BUFFER_SIZE = 512;
   private final List<Solver> solvers;
+  private final ExecutorService pool;
 
   public UDPServer() {
     this.solvers = new ArrayList<>();
+    this.pool = ThreadPool.create(5);
   }
 
   public UDPServer bind(Solver solver) {
@@ -25,6 +31,11 @@ public class UDPServer {
   }
 
   public void start(int port, InetAddress bindAddress) {
+    this.pool.submit(() -> this.start0(port, bindAddress));
+    log.info("status=starting.., port={}, bindAddress={}", port, bindAddress);
+  }
+
+  private void start0(int port, InetAddress bindAddress) {
     try {
       final var server = new DatagramSocket(port, bindAddress);
       final byte[] buff = new byte[BUFFER_SIZE];
@@ -32,20 +43,26 @@ public class UDPServer {
 
         final var in = new DatagramPacket(buff, 0, buff.length);
         server.receive(in);
-        final var response = this.handle(in).toWire();
-
-        final var out = new DatagramPacket(response, response.length);
-        out.setAddress(in.getAddress());
-        out.setPort(in.getPort());
-
-        server.send(out);
-
+        this.pool.submit(() -> this.handle(server, this.handle(in), in.getAddress(), in.getPort()));
 
       }
     } catch (IOException e) {
       throw new UncheckedIOException(e);
     }
+  }
 
+  public void handle(DatagramSocket server, Message handle, InetAddress address, int port) {
+    try {
+      final var response = handle.toWire();
+      final var out = new DatagramPacket(response, response.length);
+      out.setAddress(address);
+      out.setPort(port);
+
+      server.send(out);
+
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
   }
 
   private Message handle(DatagramPacket packet) {
