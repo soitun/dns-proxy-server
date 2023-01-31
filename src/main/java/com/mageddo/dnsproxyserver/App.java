b386d1b73777340b4dc153b79c05367e0348be15
Elvis de Freitas <edigitalb@gmail.com>
2023-01-31 00:08:12 -0300

    unifiying

--- src/main/java/com/mageddo/dnsproxyserver/App.java
+++ src/main/java/com/mageddo/dnsproxyserver/App.java
@@ -1,21 +1,23 @@
 package com.mageddo.dnsproxyserver;
 
-import com.mageddo.dnsproxyserver.server.dns.Main;
+import com.mageddo.dnsproxyserver.dagger.Factory;
 
 public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-
-    // start webserver
+  public static void main(String[] args) throws InterruptedException {
+    final var factory = Factory.factory();
 
     // start dns server
+    factory.dnsServerStarter().start();
+
+    // start webserver
 
     // configurations
 
     //  setup as default dns
+
     //  install as service
 
-    Main.main(args);
+    Thread.currentThread().join();
 
   }
 }
