1ec5de592454ae5b08231000ad04f247fe71796f
Elvis de Freitas <edigitalb@gmail.com>
2023-01-31 10:11:00 -0300

    some docs

--- src/main/java/com/mageddo/dnsproxyserver/config/flags/Main.java
+++ src/main/java/com/mageddo/dnsproxyserver/config/flags/Main.java
@@ -5,6 +5,10 @@ public class Main {
   // todo configurar o jcommander e comecar a usar
   // https://github.com/mageddo-projects/portainer-cli/blob/master/src/main/java/com/mageddo/portainer/cli/command/PortainerCommand.java
   // https://github.com/mageddo/java-examples/tree/master/command-line-parser/jcommander/src/main/java/com/mageddo/jcommander/git
+  // https://github.com/oracle/graal/issues/721
+
+  // todo talvez testar o commons cli da apache pra ver se faz mais sentido usa lo cmo ele nao usa reflection
+//  https://commons.apache.org/proper/commons-cli/usage.html
 }
 
 
