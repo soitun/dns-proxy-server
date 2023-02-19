801de952e2d1b6a8a225ad16621dd9bd4dfe9297
Elvis de Freitas <edigitalb@gmail.com>
2023-02-19 12:44:04 -0300

    need to scan whole package

--- src/main/java/com/mageddo/os/linux/struct/ReflectConfig.java
+++ src/main/java/com/mageddo/os/linux/struct/ReflectConfig.java
@@ -3,6 +3,7 @@ package com.mageddo.os.linux.struct;
 import nativeimage.Reflection;
 
 @Reflection(
+  scanPackage = "com.mageddo.os.linux.struct",
   publicConstructors = true, declaredConstructors = true, declaredFields = true, publicFields = true,
   declaredMethods = true, publicMethods = true
 )
