af76744b59888d1d37a6416a7889452c8272e759
Elvis de Freitas <edigitalb@gmail.com>
2023-02-19 14:41:34 -0300

    Fixing reference

--- src/main/java/com/mageddo/os/linux/jna/ReflectConfig.java
+++ src/main/java/com/mageddo/os/linux/jna/ReflectConfig.java
@@ -3,7 +3,7 @@ package com.mageddo.os.linux.jna;
 import nativeimage.Reflection;
 
 @Reflection(
-  scanPackage = "com.mageddo.os.linux.struct",
+  scanPackage = "com.mageddo.os.linux.jna",
   publicConstructors = true, declaredConstructors = true, declaredFields = true, publicFields = true,
   declaredMethods = true, publicMethods = true
 )
