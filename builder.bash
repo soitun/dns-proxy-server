d057a461cece2c33ca97b51d2b780c8492827d62
Elvis de Freitas <edigitalb@gmail.com>
2023-02-19 19:36:33 -0300

    Cd is probably ready

--- builder.bash
+++ builder.bash
@@ -2,158 +2,113 @@
 
 set -e
 
-CUR_DIR=`pwd`
-APP_VERSION=$(cat VERSION)
+REPO_DIR=`pwd`
+APP_VERSION=$(cat gradle.properties | grep -oP 'version=\K(.+)')
 
 echo "> builder.bash version=${APP_VERSION}"
 
-assemble(){
-	echo "> Testing ..."
-	go test -p 1 -cover -ldflags "-X github.com/mageddo/dns-proxy-server/flags.version=test" ./.../
-	echo "> Tests completed"
+copyFileFromService(){
 
-	echo "> Building..."
+  serviceName=$1
+  from=$2
+  to=$3
 
-	rm -rf build/
-	mkdir -p build/
+  docker-compose --compatibility create --build $serviceName --force-recreate 1>&2
+  id=$(docker ps -a | grep $serviceName | awk '{print $1}')
+  docker cp "$id:$from" "$to"
+}
 
-	cp -r /static build/
+applyVersion(){
+  echo "> Apply version"
+  sed -i -E "s/(dns-proxy-server.*)[0-9]+\.[0-9]+\.[0-9]+/\1$APP_VERSION/" docker-compose.yml
 }
 
 generateDocs(){
-	echo "> Generating docs version=${1}, target=${2}"
-	mkdir -p "${2}"
-	hugo --baseURL=http://mageddo.github.io/dns-proxy-server/$1 \
-	--destination $2 \
-	--ignoreCache --source docs/
+  echo "> Generating docs version=${1}, target=${2}"
+  mkdir -p "${2}"
+  hugo --baseURL=http://mageddo.github.io/dns-proxy-server/$1 \
+  --destination $2 \
+  --ignoreCache --source docs/
 
-	echo "> Generated docs version=$1, out files:"
-	ls -lha $2
+  echo "> Generated docs version=$1, out files:"
+  ls -lha $2
+}
+
+uploadRelease(){
+  echo "> upload-release "
+  DESC=$(cat RELEASE-NOTES.md | awk 'BEGIN {RS="|"} {print substr($0, 0, index(substr($0, 3), "###"))}' | sed ':a;N;$!ba;s/\n/\\r\\n/g')
+  github-cli release mageddo dns-proxy-server $APP_VERSION $CURRENT_BRANCH "${DESC}" $PWD/build/*.tgz
 }
 
 case $1 in
 
-	upload-release )
-		echo "> upload-release "
-		DESC=$(cat RELEASE-NOTES.md | awk 'BEGIN {RS="|"} {print substr($0, 0, index(substr($0, 3), "###"))}' | sed ':a;N;$!ba;s/\n/\\r\\n/g')
-		github-cli release mageddo dns-proxy-server $APP_VERSION $CURRENT_BRANCH "${DESC}" $PWD/build/*.tgz
+  docs )
 
-	;;
+    P=${2:-${PWD}/build}
+    echo "> Docs ${P}"
 
-	docs )
+    MINOR_VERSION=$(echo $APP_VERSION | awk -F '.' '{ print $1"."$2}');
+    rm -r "$2/docs" || echo "> build dir already clear"
 
-	P=${2:-${PWD}/build}
-	echo "> Docs ${P}"
+    TARGET="$2/docs/${MINOR_VERSION}"
+    generateDocs ${MINOR_VERSION} ${TARGET}
 
-	VERSION=$(cat VERSION | awk -F '.' '{ print $1"."$2}');
-	rm -r "$2/docs" || echo "> build dir already clear"
+    MINOR_VERSION=latest
+    TARGET="$2/docs/${MINOR_VERSION}"
+    generateDocs ${MINOR_VERSION} ${TARGET}
 
-	TARGET="$2/docs/${VERSION}"
-	generateDocs ${VERSION} ${TARGET}
+  ;;
 
-	VERSION=latest
-	TARGET="$2/docs/${VERSION}"
-	generateDocs ${VERSION} ${TARGET}
+  validate-release )
+    echo "> validate release, version=${APP_VERSION}, git=$(git rev-parse $APP_VERSION 2>/dev/null)"
+    if git rev-parse "$APP_VERSION^{}" >/dev/null 2>&1; then
+      echo "> Tag already exists $APP_VERSION"
+      exit 3
+    fi
+  ;;
 
-	;;
+  deploy )
 
-	apply-version )
-		echo "> Apply version"
-		# updating files version
-		sed -i -E "s/(dns-proxy-server.*)[0-9]+\.[0-9]+\.[0-9]+/\1$APP_VERSION/" docker-compose.yml
+  echo "> Deploy started , current branch=$CURRENT_BRANCH"
+  ./builder.bash validate-release
 
-	;;
+  if [ "$CURRENT_BRANCH" = "master" ]; then
+    echo "> deploying new version"
+    applyVersion && builder.bash build && builder.bash upload-release
+  else
+    echo "> refusing to go ahead outside the master branch"
+  fi
 
-	assemble )
-		echo "> assemble"
-		assemble
-	;;
-
-	build )
-
-		echo "> build"
-
-		assemble
-
-		if [ ! -z "$2" ]
-		then
-			builder.bash compile $2 $3
-			exit 0
-		fi
-
-		# ARM
-		builder.bash compile linux arm
-		builder.bash compile linux arm64
-
-		# LINUX
-		# INTEL / AMD
-		builder.bash compile linux 386
-		builder.bash compile linux amd64
-
-		echo "> Build success"
-
-	;;
-
-	compile )
-		export GOOS=$2
-		export GOARCH=$3
-		echo "> Compiling os=${GOOS}, arch=${GOARCH}"
-		go build -o $PWD/build/dns-proxy-server -ldflags "-X github.com/mageddo/dns-proxy-server/flags.version=$APP_VERSION"
-		TAR_FILE=dns-proxy-server-${GOOS}-${GOARCH}-${APP_VERSION}.tgz
-		cd $PWD/build/
-		tar --exclude=*.tgz -czf $TAR_FILE *
-	;;
-
-	validate-release )
-		echo "> validate release, version=${APP_VERSION}, git=$(git rev-parse $APP_VERSION 2>/dev/null)"
-		if git rev-parse "$APP_VERSION^{}" >/dev/null 2>&1; then
-			echo "> Tag already exists $APP_VERSION"
-			exit 3
-		fi
-	;;
-
-	deploy )
-
-	echo "> Deploy"
-	./builder.bash validate-release
 
   echo "> Building frontend files..."
-  docker-compose --compatibility create --build build-frontend --force-recreate &&\
-   id=$(docker ps -a | grep build-frontend | awk '{print $1}') &&\
-   docker cp $id:/static ./src/main/resources/META-INF/resources/static
+  copyFileFromService build-frontend /static ./src/main/resources/META-INF/resources/static
 
-	echo "> Build, test and generate the binaries to the output dir"
+  echo "> Build, test and generate the binaries"
 
+  OS=linux
+  ARCH=amd64
+  SERVICE_NAME="build-${OS}-ARCH"
+  BIN_FILE="./build/dns-proxy-server-${OS}-${ARCH}-${APP_VERSION}"
+  TAR_FILE=${BIN_FILE}.tgz
 
+  docker-compose build --progress=plain ${SERVICE_NAME}
+  copyFileFromService ${SERVICE_NAME} /app/dns-proxy-server ${BIN_FILE}
+  cd $PWD/build/
+  tar --exclude=*.tgz -czf $TAR_FILE ${BIN_FILE}
 
-	EC=0
-	docker-compose up --force-recreate --abort-on-container-exit prod-ci-deploy || EC=$?
-	if [ "$EC" = "3" ]; then
-		exit 0
-	elif [ "$EC" -ne "0" ]; then
-		exit $EC
-	fi
+  echo "> Uploading the release artifacts"
+  cd $REPO_DIR
+  uploadRelease
 
-	echo "> From the binaries, build the docker images then push them to docker hub"
-	docker-compose build prod-build-image-dps prod-build-image-dps-arm7x86 prod-build-image-dps-arm8x64 &&\
-	docker tag defreitas/dns-proxy-server:${APP_VERSION} defreitas/dns-proxy-server:latest &&\
+	echo "> Push docker images to docker hub"
+#	docker-compose build prod-build-image-dps prod-build-image-dps-arm7x86 prod-build-image-dps-arm8x64 &&\
+#	docker tag defreitas/dns-proxy-server:${APP_VERSION} defreitas/dns-proxy-server:latest &&\
 	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin &&\
-	docker-compose push prod-build-image-dps prod-build-image-dps-arm7x86 prod-build-image-dps-arm8x64 &&
-	docker push defreitas/dns-proxy-server:latest
+	docker-compose push build-linux-amd64
+#	docker-compose push prod-build-image-dps prod-build-image-dps-arm7x86 prod-build-image-dps-arm8x64 &&
+#	docker push defreitas/dns-proxy-server:latest
 
 	;;
 
-	release )
-
-		echo "> build started, current branch=$CURRENT_BRANCH"
-		if [ "$CURRENT_BRANCH" = "master" ]; then
-			echo "> deploying new version"
-			builder.bash validate-release && builder.bash apply-version && builder.bash build && builder.bash upload-release
-
-		else
-			echo "> refusing to keep going outside the master branch"
-		fi
-
-	;;
 
 esac
