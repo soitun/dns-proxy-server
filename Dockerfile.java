257bea2f0d11751a42d01d089d720e7712fe713e
Elvis Souza <edigitalb@gmail.com>
2023-02-20 02:13:45 -0300

    replace with java stuff (#287)

--- Dockerfile.java
+++
@@ -1,6 +0,0 @@
-FROM openjdk:17-slim
-COPY ./build/dns-proxy-server-*-alpha-runner.jar /app/dns-proxy-server.jar
-COPY conf/ /app/conf/
-RUN #mkdir /app/conf
-WORKDIR /app
-ENTRYPOINT ["java", "-jar", "/app/dns-proxy-server.jar", "--log-level", "DEBUG"]
