92900ba2995baf7dae958fcc2a46a0dc9c57471e
Elvis Souza <edigitalb@gmail.com>
2024-08-06 22:51:08 -0300

    caching in test method, leading with usecase when a second thread is running the test (#541)

--- src/main/java/com/mageddo/utils/Tests.java
+++ src/main/java/com/mageddo/utils/Tests.java
@@ -1,12 +1,54 @@
 package com.mageddo.utils;
 
+import com.mageddo.commons.lang.Singletons;
+
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+
 public class Tests {
+
+  private static final String JUNIT_FRAMEWORK_PACKAGE = "org.junit.";
+  private static final AtomicInteger hotCallsStat = new AtomicInteger();
+  private static final Class<Tests> CACHE_KEY = Tests.class;
+
   public static boolean inTest() {
-    for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
-      if (element.getClassName().startsWith("org.junit.")) {
+    return Singletons.createOrGet(CACHE_KEY, Tests::inTestHotLoad);
+  }
+
+  static boolean inTestHotLoad() {
+    hotCallsStat.incrementAndGet();
+    return findAllThreads()
+      .stream()
+      .anyMatch(Tests::hashJunitInStackTrace);
+  }
+
+  private static Set<Thread> findAllThreads(){
+    return Thread.getAllStackTraces().keySet();
+  };
+
+  private static boolean hashJunitInStackTrace(final Thread thread) {
+    return hashClassInStackTrace(
+      thread,
+      (element) -> element.getClassName().startsWith(JUNIT_FRAMEWORK_PACKAGE)
+    );
+  }
+
+  private static boolean hashClassInStackTrace(Thread thread, final Predicate<StackTraceElement> p) {
+    for (final var element : thread.getStackTrace()) {
+      if (p.test(element)) {
         return true;
       }
     }
     return false;
+  }
+
+  static int getHotCallsStat() {
+    return hotCallsStat.get();
+  }
+
+  static void resetCache(){
+    hotCallsStat.set(0);
+    Singletons.clear(CACHE_KEY);
   }
 }
---
+++ src/test/java/com/mageddo/utils/TestsTest.java
@@ -0,0 +1,46 @@
+package com.mageddo.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TestsTest {
+
+  @BeforeEach
+  void beforeEach(){
+    Tests.resetCache();
+  }
+
+  @Test
+  void mustCacheInTestCalls(){
+
+    for (int i = 0; i < 3; i++) {
+      assertTrue(Tests.inTest());
+    }
+
+    assertEquals(1, Tests.getHotCallsStat());
+  }
+
+  @Test
+  void mustBeJunitTest(){
+    assertTrue(Tests.inTestHotLoad());
+  }
+
+  @Test
+  void hotCallsAreNotCached(){
+    for (int i = 0; i < 3; i++) {
+      assertTrue(Tests.inTestHotLoad());
+    }
+    assertEquals(Tests.getHotCallsStat(), 3);
+  }
+
+  @Test
+  void mustBeJunitTestEvenWhenRunningInBackground() throws Exception {
+    try(final var executor = Executors.newThreadExecutor()){
+      final var task = executor.submit(Tests::inTestHotLoad);
+      assertTrue(task.get());
+    }
+  }
+}
