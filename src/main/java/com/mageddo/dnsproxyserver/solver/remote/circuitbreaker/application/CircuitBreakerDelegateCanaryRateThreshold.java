fcc618219e9d95290c6ddc45fb080b0e47b57b96
Elvis de Freitas <edigitalb@gmail.com>
2024-09-09 10:41:37 -0300

    more tests

--- src/main/java/com/mageddo/dnsproxyserver/solver/remote/circuitbreaker/application/CircuitBreakerDelegateCanaryRateThreshold.java
+++ src/main/java/com/mageddo/dnsproxyserver/solver/remote/circuitbreaker/application/CircuitBreakerDelegateCanaryRateThreshold.java
@@ -1,7 +1,9 @@
 package com.mageddo.dnsproxyserver.solver.remote.circuitbreaker.application;
 
+import com.mageddo.commons.circuitbreaker.CircuitIsOpenException;
 import com.mageddo.dnsproxyserver.solver.remote.CircuitStatus;
 import com.mageddo.dnsproxyserver.solver.remote.Result;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 
 import java.util.function.Supplier;
@@ -16,7 +18,11 @@ public class CircuitBreakerDelegateCanaryRateThreshold implements CircuitBreaker
 
   @Override
   public Result execute(Supplier<Result> sup) {
-    return this.circuitBreaker.executeSupplier(sup);
+    try {
+      return this.circuitBreaker.executeSupplier(sup);
+    } catch (CallNotPermittedException e){
+      throw new CircuitIsOpenException(e);
+    }
   }
 
   @Override
--- src/test/java/com/mageddo/dnsproxyserver/solver/remote/circuitbreaker/application/CircuitBreakerDelegateCanaryRateThresholdTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/solver/remote/circuitbreaker/application/CircuitBreakerDelegateCanaryRateThresholdTest.java
@@ -1,7 +1,58 @@
 package com.mageddo.dnsproxyserver.solver.remote.circuitbreaker.application;
 
+import com.mageddo.commons.circuitbreaker.CircuitCheckException;
+import com.mageddo.commons.circuitbreaker.CircuitIsOpenException;
+import com.mageddo.dnsproxyserver.solver.remote.Result;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import testing.templates.circuitbreaker.Resilience4jCircuitBreakerTemplates;
+import testing.templates.solver.remote.ResultTemplates;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class CircuitBreakerDelegateCanaryRateThresholdTest {
 
   CircuitBreakerDelegateCanaryRateThreshold delegate;
 
+  @BeforeEach
+  void beforeEach(){
+    this.delegate = new CircuitBreakerDelegateCanaryRateThreshold(Resilience4jCircuitBreakerTemplates.fastFail());
+  }
+
+  @Test
+  void mustExecuteSupplierWithSuccess(){
+    // arrange
+    final var counter = new AtomicInteger();
+
+    // act
+    final var result = this.delegate.execute(() -> {
+      counter.incrementAndGet();
+      return ResultTemplates.success();
+    });
+
+    // assert
+    assertNotNull(result);
+    assertTrue(result.hasSuccessMessage());
+    assertEquals(1, counter.get());
+  }
+
+
+  @Test
+  void mustThrowSpecificExceptionWhenGetOpenCircuit(){
+    // arrange
+    final Supplier<Result> sup = () -> {
+      throw new CircuitCheckException("Mocked Error");
+    };
+
+    // act // assert
+    assertThrows(CircuitCheckException.class, () -> this.delegate.execute(sup));
+    assertThrows(CircuitIsOpenException.class, () -> this.delegate.execute(sup));
+
+  }
 }
